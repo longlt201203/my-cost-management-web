f12afc8915499c8d1899719f105725a90a8d46bd
Le Thanh Long <longlt201203@gmail.com>
2024-12-05 17:51:39 +0700

    feat: 🎸 update

--- src/pages/Dashboard/Boards/index.tsx
+++ src/pages/Dashboard/Boards/index.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 
 const { Title } = Typography;
 
@@ -6,6 +6,26 @@ export default function DashboardBoardsPage() {
   return (
     <div className="p-4">
       <Title level={2}>Boards</Title>
+      <Row gutter={[16, 16]}>
+        {Array.from({ length: 15 }, (_, index) => (
+          <Col
+            xs={{ span: 24 }}
+            md={{ span: 12 }}
+            lg={{ span: 8 }}
+            xl={{ span: 6 }}
+          >
+            <Card
+              key={index}
+              cover={<img src="https://placehold.co/600x400" />}
+              bordered={false}
+              hoverable
+              onClick={() => {}}
+            >
+              <Card.Meta title="Card Title" description="Board description" />
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   );
 }

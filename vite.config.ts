07ae7f12ccebac6d26f193266b55a44fd581374b
Phan Long <longbp166@gmail.com>
2024-12-10 15:35:15 +0700

    refactor: 💡 api url

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     host: "0.0.0.0",
     proxy: {
       "^/api": {
-        target: "https://mcmapp.online",
+        target: "http://mcmapp.online",
         changeOrigin: true,
       },
     },
